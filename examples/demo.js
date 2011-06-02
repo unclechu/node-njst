0002ef7f42b91b583109db1a32a96e89ccdf843d
unclechu <lotsmanov89@gmail.com>
2011-06-02 18:25:36 +0600

    Many changes. To public.

--- examples/demo.js
+++ examples/demo.js
@@ -2,23 +2,25 @@ var njst = require('../lib/njst');
 var fs = require('fs');
 var http = require('http');
 
-http.createServer(function (request, response) {
+http.createServer(function (request, response) {	
 	fs.readFile('../templates/demo.html', function (err, data) {
 		if (err) return;
 		
+		var out;
 		var context = {
-			PageTitle: 'jJSt testing',
-			list: [
-				{name:'First', description:'it\'s a first step'},
-				{name:'Second', description:'ok, next', list: [1,2,3,4,'hahahoho']},
-				{name:'End', description:'last step'},
+			PageTitle: 'jJSt demonstration #1',
+			List: [
+				{Name:'First', Description:'it\'s a first step'},
+				{Name:'Second', Description:'ok, next', List: ['One', 'Two', 'Three']},
+				{Name:'End', Description:'last step'},
 			],
-			ShowIf: true,
-			//show: 123,
-			//toShow: 123,
+			ShowCopyright: true,
+			ShowHelloWorld: true,
+			HelloWorld: 'Hello world! Yeah!',
 		};
 		
+		out = njst.parse(data, context, {debug:1});
 		response.writeHead(200, {'content-type':'text/html; charset=utf-8'});
-		response.end(njst.parse(data, context, {debug:1}));
+		response.end(out);
 	});
 }).listen(8000);
---
+++ examples/demo2.js
@@ -0,0 +1,33 @@
+var njst = require('../lib/njst');
+var fs = require('fs');
+var http = require('http');
+
+http.createServer(function (request, response) {	
+	fs.readFile('../templates/demo2.html', function (err, data) {
+		if (err) return;
+		
+		var out;
+		var download = false;
+		
+		var context = {
+			PageTitle: 'jJSt demonstration #2',
+			DownloadPage: function () {
+				download = true;
+			},
+		};
+		
+		out = njst.parse(data, context);
+		
+		if (download) {
+			response.writeHead(200, {
+				'content-type': 'application/octet-stream; charset=utf-8',
+				'content-disposition': 'attachment; filename="page.html"',
+				'accept-ranges': 'bytes',
+				'content-length': Buffer.byteLength(out),
+			});
+		} else {
+			response.writeHead(200, {'content-type':'text/html; charset=utf-8'});
+		}
+		response.end(out);
+	});
+}).listen(8000);
